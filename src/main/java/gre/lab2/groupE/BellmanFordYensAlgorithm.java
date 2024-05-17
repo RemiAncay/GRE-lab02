48373bf0032e4141f157a7c919effac7e2fddfc3
borishuu <boris.hutzli@heig-vd.ch>
2024-05-17 14:39:50 +0200

    finished comments

--- src/main/java/gre/lab2/groupE/BellmanFordYensAlgorithm.java
+++ src/main/java/gre/lab2/groupE/BellmanFordYensAlgorithm.java
@@ -6,21 +6,44 @@ import gre.lab2.graph.WeightedDigraph;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
 import java.util.Queue;
 
+/**
+ * Represents the BellmanFordYens algorithm
+ *
+ * @author Ançay Rémi
+ * @author Hutzli Boris
+ */
 public final class BellmanFordYensAlgorithm implements IBellmanFordYensAlgorithm {
+
+  /** Sentinel vertex value */
+  private final int SENTINEL = -2;
+
+  /** Infinite value */
+  private final int INFINITE = Integer.MAX_VALUE;
+
+  /** Unreachable vertex value */
+  private final int UNREACHABLE = -1;
+
   @Override
   public BFYResult compute(WeightedDigraph graph, int from) {
-    final int SENTINEL = -2;
-    final int INFINITE = Integer.MAX_VALUE;
-    final int UNREACHABLE = -1;
 
-    //Bellman Ford Yens Algorythme
+    // Array of predecessors
     int[] predecessors = new int[graph.getNVertices()];
-    int[] distances = new int[graph.getNVertices()];
-    int interCounter = 0;
 
+    // Array of distances to the source
+    int[] distances = new int[graph.getNVertices()];
+
+    // Iteration counter
+    int iterationCounter = 0;
+
+    //Queue to store the vertices to iterate
+    Queue<Integer> verticesToIterate = new ArrayDeque<>();
+
+    // Determines whether the vertex are currently in the verticesToIterate queue
+    boolean[] visitedVerticeToIterate = new boolean[graph.getNVertices()];
+
+    // Initialises each vertex distance and predecessor
     for (int j = 0; j < graph.getNVertices(); j++)
     {
       distances[j] = INFINITE;
@@ -28,29 +51,43 @@ public final class BellmanFordYensAlgorithm implements IBellmanFordYensAlgorithm
     }
     distances[from] = 0;
 
-    Queue<Integer> verticesToIterate = new ArrayDeque<>(); //Queue to store the vertices to iterate
-    boolean[] visitedVerticeToIterate = new boolean[graph.getNVertices()];
-
-
+    // Adds source the queue
     verticesToIterate.add(from);
     visitedVerticeToIterate[from] = true;
+
+    // Adds the sentinel
     verticesToIterate.add(SENTINEL);
 
+    // Apply the BFY algorithm
     while (!verticesToIterate.isEmpty())
     {
       int vertex = verticesToIterate.remove();
+
+      // If the sentinel is found, increment iteration
       if (vertex == SENTINEL)
       {
+        // Continue the algorithm as long as there remains vertices to iterate
         if(!verticesToIterate.isEmpty())
         {
-          interCounter++;
-          if(interCounter == graph.getNVertices()) //If true : There is a negative cycle
-          {
-            //Backtracking the predecessors to find the negative cycle
-            Queue<Integer> verticesFound = new ArrayDeque<>(); //Queue to store the vertices found
-            boolean[] visited = new boolean[graph.getNVertices()]; //Array to store the visited vertices
+          iterationCounter++;
 
-            int vertexFound = verticesToIterate.remove();
+          // If the amount of iterations done is equal to the amount of vertices, there is a negative cycle
+          if(iterationCounter == graph.getNVertices())
+          {
+            //Queue to store the vertices found
+            Queue<Integer> verticesFound = new ArrayDeque<>();
+
+            //Array to store the visited vertices
+            boolean[] visited = new boolean[graph.getNVertices()];
+
+            // Vertex found when backtracking the cycle
+            int vertexFound;
+
+            //Storing the negative cycle
+            ArrayList<Integer> negativeCycle = new ArrayList<>();
+
+            // Initialise the vertex found
+            vertexFound = verticesToIterate.remove();
             verticesFound.add(vertexFound);
             visited[vertexFound] = true;
 
@@ -66,9 +103,8 @@ public final class BellmanFordYensAlgorithm implements IBellmanFordYensAlgorithm
               visited[vertexFound] = true;
             }
 
-            //Storing the negative cycle
-            ArrayList<Integer> negativeCycle = new ArrayList<>();
-            while (vertexFound != verticesFound.peek())
+            // Remove the vertices that aren't part of the negative cycle
+            while (!verticesFound.isEmpty() && vertexFound != verticesFound.peek())
             {
               verticesFound.remove();
             }
@@ -78,7 +114,7 @@ public final class BellmanFordYensAlgorithm implements IBellmanFordYensAlgorithm
               negativeCycle.addFirst(verticesFound.remove());
             }
 
-            //Calculating the negative cycle length
+            // Calculating the negative cycle length
             int negativeCycleLength = 0;
             for (int i = 0; i < negativeCycle.size(); i++)
             {
@@ -94,16 +130,19 @@ public final class BellmanFordYensAlgorithm implements IBellmanFordYensAlgorithm
 
             return new BFYResult.NegativeCycle(negativeCycle, negativeCycleLength);
           }
+          // Add back the sentinel for the next iteration
           else
           {
             verticesToIterate.add(SENTINEL);
           }
         }
       }
+      // If no sentinel found, continue the iteration
       else
       {
         visitedVerticeToIterate[vertex] = false;
 
+        // Calculates the distances and predecessors and re-iterates the successors if there was a distance upgrade
         for (WeightedDigraph.Edge successor : graph.getOutgoingEdges(vertex))
         {
           if(distances[successor.to()] > distances[vertex] + successor.weight())
